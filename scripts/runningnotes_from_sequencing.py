6be7ccd66ae0828826af9e7bf02c4db0caa92e1a
Anandashankar Anil <anandasanil@gmail.com>
2025-03-31 17:47:38 +0200

    Rewrite to accommodate a project being in multiple lanes

--- scripts/runningnotes_from_sequencing.py
+++ scripts/runningnotes_from_sequencing.py
@@ -2,6 +2,7 @@
 DESC = """EPP script to summarize sequencing start results to the projects running notes
 """
 import datetime
+import logging
 import re
 from argparse import ArgumentParser
 
@@ -12,9 +13,20 @@ from write_notes_to_couchdb import write_note_to_couch
 
 from scilifelab_epps.wrapper import epp_decorator
 
-regex_projectid_line = re.compile(r"^\[(P\d+)\][\s]*:")
-# regex_projectname_line = re.compile(r"^\[([A-Za-z]+\.[A-Za-z]+_\d{2}_\d{2})\][\s]*:")
+regex_projectid_line = re.compile(r"^(P\d+)[\s]*:")
+# regex_projectname_line = re.compile(r"^([A-Za-z]+\.[A-Za-z]+_\d{2}_\d{2})[\s]*:")
 TIMESTAMP: str = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+"""
+Example running note
+
+Comment from Load to Flowcell (NovaSeqXPlus) v1.0 (LIMS) :
+Sequencing started 2025-03-31
+Pool 'POOLNAME' in lane 1, xpM, y% PhiX,
+Pool 'POOLNAME' in lane 2, xpM, y% PhiX,
+1.5B FC=22XDFCRT, on Inst A, 1_3_4_2
+
+/Technician Name
+"""
 
 
 @epp_decorator(script_path=__file__, timestamp=TIMESTAMP)
@@ -22,19 +34,45 @@ def main(args):
     lims = Lims(BASEURI, USERNAME, PASSWORD)
     pro = Process(lims, id=args.pid)
     pro_udfs = {}
-    samples = {}
+    pools = {}
     general_comments = []
     project_specific_comments = {}
+    projects = {}
 
     for name, value in pro.udf.items():
         pro_udfs[name] = value
-    seq_setup = f"{pro_udfs.get('Read 1 Cycles')}_{pro_udfs.get('Index Read 1')}_{pro_udfs.get('Index Read 2')}_{pro_udfs.get('Read 2 Cycles')}"
-    inst = f"{pro_udfs.get('Instrument')} {pro_udfs.get('Side')}"
+    # Use get for non mandatory UDFs
+    seq_setup = f"{pro_udfs['Read 1 Cycles']}_{pro_udfs['Index Read 1']}_{pro_udfs.get('Index Read 2', 'x')}_{pro_udfs.get('Read 2 Cycles', 'x')}"
+    inst = f"{pro_udfs['Instrument']} {pro_udfs['Side']}"
 
+    date_started = datetime.datetime.fromisoformat(pro.step.date_started).date()
+
+    pool_artifacts = pro.analytes()[0]
+    for art in pool_artifacts:
+        pool_obj = {}
+        for name, value in art.udf.items():
+            pool_obj[name] = value
+
+        pool_obj["pool_name"] = art.name
+        pools[art.id] = pool_obj
+        seen_projects = set()
+        for sample in art.samples:
+            if sample.project.id not in projects:
+                projects[sample.project.id] = []
+            if sample.project.id not in seen_projects:
+                projects[sample.project.id].append(art.id)
+                seen_projects.add(sample.project.id)
+
+    # The default value for the Comment field in LIMS contains lines that start with '//' which should be filtered out if they are not removed
+    # They could also be used to save info in the step that does not have to be in Genstat
     for line in pro_udfs.get("Comment").splitlines():
         if not line.startswith("//"):
             if regex_projectid_line.match(line):
                 result = regex_projectid_line.search(line)
+                if result.group(1) not in projects:
+                    logging.warning(
+                        f"Project {result.group(1)} not in list of projects for this run. Skipping comment."
+                    )
                 if result.group(1) not in project_specific_comments:
                     project_specific_comments[result.group(1)] = []
                 project_specific_comments[result.group(1)].append(
@@ -43,55 +81,45 @@ def main(args):
             else:
                 general_comments.append(line.strip())
 
-    date_started = datetime.datetime.fromisoformat(pro.step.date_started).date()
-
-    sample_artifacts = pro.analytes()[0]
-    for art in sample_artifacts:
-        sample_item = {}
-        sample_item["projects"] = set()
-        for sample in art.samples:
-            sample_item["projects"].add(sample.project.id)
-
-        for name, value in art.udf.items():
-            sample_item[name] = value
-
-        sample_item["pool"] = art.name
-        samples[art.id] = sample_item
-
     an_analyte_container = pro.output_containers()[0]
     container_name = an_analyte_container.name
     container_type = an_analyte_container.type.name
     for well, art in an_analyte_container.placements.items():
-        if art.id in samples:
-            samples[art.id]["lane"] = well.split(":")[0]
+        if art.id in pools:
+            pools[art.id]["lane"] = well.split(":")[0]
 
     note_creation_date = datetime.datetime.now()
-    kit_size = ""  # TODO: get kit size
-    for sample in samples.values():
-        for project in sample["projects"]:
-            note_obj = {
-                "_id": f"{project}:{datetime.datetime.timestamp(note_creation_date)}",
-                "categories": ["Lab"],
-                "note_type": "project",
-                "parent": project,
-                "created_at_utc": note_creation_date.isoformat(),
-                "updated_at_utc": note_creation_date.isoformat(),
-                "projects": [project],
-                "user": pro.technician.name,
-                "email": pro.technician.email,
-            }
-            project_comments = "\n".join(project_specific_comments.get(project, []))
-            # project_comments = "\n".join(project_specific_comments.get(Project(lims, id=project).name, []))
-            note_obj["note"] = (
-                f"Comment from {pro.type.name} ([LIMS]({BASEURI}/clarity/work-details/{pro.id.split('-')[1]})) : \n \
-                                **Sequencing started {date_started} ** \n \
-                                Pool {sample['pool']} in lane {sample['lane']}, {sample['Loading Conc. (pM)']}pM, {sample['% phiX']}% PhiX, \n \
-                                {container_type}-{kit_size}  FC={container_name}, on {inst}, {seq_setup} \n \
-                                {project_comments} \n \
-                                {'/n'.join(general_comments)} \n \
-                                /{pro.technician.name}"
-            )
-            write_note_to_couch(project, note_creation_date, note_obj, BASEURI)
+    # kit_size = ""  # TODO: get kit size
+    for project in projects:
+        project_comments = "\n".join(project_specific_comments.get(project, []))
+        # project_comments = "\n".join(project_specific_comments.get(Project(lims, id=project).name, []))
+        pool_text = ""
+        for pool_id in projects[project]:
+            pool = pools[pool_id]
+            pool_text += f"Pool '{pool['pool_name']}' in lane {pool['lane']}, {pool['Loading Conc. (pM)']}pM, {pool['% phiX']}% PhiX, \n"
+        # TODO: get kit size
+        note = (
+            f"Comment from {pro.type.name} ([LIMS]({BASEURI}/clarity/work-details/{pro.id.split('-')[1]})) : \n"
+            f"**Sequencing started {date_started} ** \n"
+            f"{pool_text}"
+            f"{container_type} FC={container_name}, on {inst}, {seq_setup} \n"
+            f"{project_comments} \n"
+            f"{'/n'.join(general_comments)} \n"
+            f"/{pro.technician.name}"
+        )
+        note_obj = {
+            "_id": f"{project}:{datetime.datetime.timestamp(note_creation_date)}",
+            "categories": ["Lab"],
+            "note_type": "project",
+            "parent": project,
+            "created_at_utc": note_creation_date.isoformat(),
+            "updated_at_utc": note_creation_date.isoformat(),
+            "projects": [project],
+            "user": pro.technician.name,
+            "email": pro.technician.email,
+            "note": note,
+        }
+        write_note_to_couch(project, note_creation_date, note_obj, BASEURI)
 
 
 if __name__ == "__main__":
