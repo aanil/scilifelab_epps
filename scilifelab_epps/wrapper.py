553e49d0d46b11717365ec377a947f8f5a556d92
kedhammar <alfred.kedhammar@scilifelab.se>
2025-08-20 13:42:05 +0200

    HMMM

--- scilifelab_epps/wrapper.py
+++ scilifelab_epps/wrapper.py
@@ -104,49 +104,49 @@ def epp_decorator(script_path: str, timestamp: str):
             )
             logging.info(f"Script called with arguments: \n\t{args_str}")
 
-            # Run
+            # Run EPP, check for errors
+            script_exception = None
             try:
                 script_main(args)
-
-            # On script error
             except Exception as e:
                 # Post error to LIMS GUI
-                logging.error(str(e), exc_info=True)
-                logging.shutdown()
+                script_exception = e
+                logger.error(e)
+
+            # Check for error / warning messages in log
+            errors_or_warnings_in_log = logger.errors_or_warnings
+            logging.shutdown()
+
+            # Check for log upload success
+            try:
                 upload_file(
                     file_path=log_filename,
                     file_slot=args.log,
                     process=process,
                     lims=lims,
                     remove=True,
-                    fail_on_missing_file_slot=False,
                 )
-                sys.stderr.write(str(e))
-                sys.exit(2)
+                log_upload_failed = True
+            except:
+                log_upload_failed = False
 
-            # On script success
+            # Build final message
+            sentences = []
+            if script_exception:
+                sentences.append("Script failed.")
             else:
-                logging.info("Script finished successfully. Uploading log file.")
-                errors_or_warnings = logger.errors_or_warnings
-                logging.shutdown()
+                sentences.append("Script finished successfully.")
+            if errors_or_warnings_in_log:
+                sentences.append("Errors or warnings occurred, please check log.")
+            if log_upload_failed:
+                sentences.append("Log upload failed.")
 
-                upload_file(
-                    file_path=log_filename,
-                    file_slot=args.log,
-                    process=process,
-                    lims=lims,
-                    remove=True,
-                    fail_on_missing_file_slot=False,
-                )
-                # Check log for errors and warnings
-                if errors_or_warnings:
-                    sys.stderr.write(
-                        "Script finished successfully, but log contains errors or warnings, please have a look."
-                    )
-                    sys.exit(2)
-                else:
-                    sys.stdout.write("Script finished successfully.")
-                    sys.exit(0)
+            # Post final message and exit
+            sys.stderr.write(" ".join(sentences))
+            if errors_or_warnings_in_log:
+                sys.exit(1)
+            else:
+                sys.exit(0)
 
         return epp_wrapper
 
