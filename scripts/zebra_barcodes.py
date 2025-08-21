f9d45542d1a7b6af533f4f2df96911bd41209b7e
kedhammar <alfred.kedhammar@scilifelab.se>
2025-08-21 13:23:39 +0200

    ai refactoring and docs

--- scripts/zebra_barcodes.py
+++ scripts/zebra_barcodes.py
@@ -14,108 +14,84 @@ from scilifelab_epps.wrapper import epp_decorator
 TIMESTAMP = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
 
 
+def build_zpl_format(format_lines, data_lines, copies=1):
+    """Helper to build ZPL format and data for a label."""
+    lines = []
+    lines.append("^XA")  # Start format definition
+    lines.extend(format_lines)  # Add format (layout) commands
+    lines.append("^XZ")  # End format definition
+    for _ in range(copies):
+        lines.append("^XA")  # Start label instance
+        lines.append("^XFFORMAT^FS")  # Recall the format defined above
+        lines.extend(data_lines)  # Add data fields for this label
+        lines.append("^XZ")  # End label instance
+    return lines
+
+
 def make_container_label(plateid, copies=1):
     """Construct label with container id as human readable and barcode"""
-    lines = []
-    lines.append("^XA")  # start of label
-    # download and store format, name of format,
-    # end of field data (FS = field stop)
-    lines.append("^DFFORMAT^FS")
-    lines.append("^LH0,0")  # label home position (label home = LH)
-    # AF = assign font F, field number 1 (FN1),
-    # print text at position field origin (FO) rel. to home
-    lines.append("^FO360,30^AFN 78,39^FN1^FS")
-    # BC=barcode 128, field number 2, Normal orientation,
-    # height 70, no interpretation line.
-    lines.append("^FO70,10^BCN,70,N,N^FN2^FS")
-    lines.append("^XZ")  # end format
-
-    for _ in range(copies):
-        lines.append("^XA")  # start of label format
-        lines.append("^XFFORMAT^FS")  # label home position
-        lines.append("^FN1^FD" + plateid + "^FS")  # this is readable
-        lines.append("^FN2^FD" + plateid + "^FS")  # this is also readable
-        lines.append("^XZ")
-    return lines
+    format_lines = [
+        "^DFFORMAT^FS",  # Delete previous format named FORMAT
+        "^LH0,0",  # Set label home position
+        "^FO360,30^AFN 78,39^FN1^FS",  # Field origin, font, field number 1 (human readable)
+        "^FO70,10^BCN,70,N,N^FN2^FS",  # Field origin, barcode, field number 2 (barcode)
+    ]
+    data_lines = [
+        f"^FN1^FD{plateid}^FS",  # Assign plateid to field 1 (human readable)
+        f"^FN2^FD{plateid}^FS",  # Assign plateid to field 2 (barcode)
+    ]
+    return build_zpl_format(format_lines, data_lines, copies)
 
 
 def makeContainerNameBarcode(plate_name, copies=1):
     """Construct label with container name as human readable"""
-    lines = []
-    lines.append("^XA")  # start of label
-    # download and store format, name of format,
-    # end of field data (FS = field stop)
-    lines.append("^DFFORMAT^FS")
-    lines.append("^LH0,0")  # label home position (label home = LH)
-    # AF = assign font F, field number 1 (FN1),
-    # print text at position field origin (FO) rel. to home
+    format_lines = [
+        "^DFFORMAT^FS",  # Delete previous format named FORMAT
+        "^LH0,0",  # Set label home position
+    ]
+    # Adjust font size and position based on name length
     if len(plate_name) > 21:
-        # Use smaller font, fits 28 chars
-        lines.append("^FO20,40^AFN 54,30^FN1^FS")
+        format_lines.append("^FO20,40^AFN 54,30^FN1^FS")  # Smaller font for long names
     else:
-        # Use larger font, fits 21 chars
-        lines.append("^FO20,30^AFN 78,39^FN1^FS")
-
-    lines.append("^XZ")  # end format
-
-    for _ in range(copies):
-        lines.append("^XA")  # start of label format
-        lines.append("^XFFORMAT^FS")  # label home position
-        lines.append("^FN1^FD" + plate_name + "^FS")  # this is readable
-        lines.append("^XZ")
-    return lines
+        format_lines.append("^FO20,30^AFN 78,39^FN1^FS")  # Larger font for short names
+    data_lines = [
+        f"^FN1^FD{plate_name}^FS"  # Assign plate_name to field 1 (human readable)
+    ]
+    return build_zpl_format(format_lines, data_lines, copies)
 
 
 def makeOperatorAndDateBarcode(operator, date, copies=1):
     """Construct label with operator name and date in human readable format"""
-    lines = []
-    lines.append("^XA")  # start of label
-    # Download and store format, name of format,
-    # end of field data (FS = field stop)
-    lines.append("^DFFORMAT^FS")
-    lines.append("^LH0,0")  # label home position (label home = LH)
-    # AF = assign font F, field number 1 (FN1),
-    # print text at position field origin (FO) rel. to home
-    lines.append("^FO420,35^ADN,36,20^FN1^FS")
-    lines.append("^FO20,35^ADN,36,20^FN2^FS")
-    lines.append("^XZ")  # end format
-
+    format_lines = [
+        "^DFFORMAT^FS",  # Delete previous format named FORMAT
+        "^LH0,0",  # Set label home position
+        "^FO420,35^ADN,36,20^FN1^FS",  # Field for date (right side)
+        "^FO20,35^ADN,36,20^FN2^FS",  # Field for operator (left side)
+    ]
     if len(operator) > 19:
-        operator = operator[:19]  # If string is longer, it would cover the date
-    for _ in range(copies):
-        lines.append("^XA")  # start of label format
-        lines.append("^XFFORMAT^FS")  # label home position
-        lines.append("^FN1^FD" + date + "^FS")  # this is readable
-        lines.append("^FN2^FD" + operator + "^FS")  # this is also readable
-        lines.append("^XZ")
-    return lines
+        operator = operator[:19]  # Truncate operator name if too long
+    data_lines = [
+        f"^FN1^FD{date}^FS",  # Assign date to field 1
+        f"^FN2^FD{operator}^FS",  # Assign operator to field 2
+    ]
+    return build_zpl_format(format_lines, data_lines, copies)
 
 
 def makeProcessNameBarcode(process_name, copies=1):
-    """Constrcut label with process name as human readable"""
-    lines = []
-    lines.append("^XA")  # start of label
-    # download and store format, name of format,
-    # end of field data (FS = field stop)
-    lines.append("^DFFORMAT^FS")
-    lines.append("^LH0,0")  # label home position (label home = LH)
-    # AF = assign font F, field number 1 (FN1),
-    # print text at position field origin (FO) rel. to home
+    """Construct label with process name as human readable"""
+    format_lines = [
+        "^DFFORMAT^FS",  # Delete previous format named FORMAT
+        "^LH0,0",  # Set label home position
+    ]
+    # Adjust font size and position based on process name length
     if len(process_name) > 21:
-        # Use smaller font, fits 28 chars
-        lines.append("^FO20,40^ADN 54,30^FN1^FS")
+        format_lines.append("^FO20,40^ADN 54,30^FN1^FS")  # Smaller font for long names
     else:
-        # Use larger font, fits 21 chars
-        lines.append("^FO20,30^AFN 78,39^FN1^FS")
-
-    lines.append("^XZ")  # end format
-
-    for _ in range(copies):
-        lines.append("^XA")  # start of label format
-        lines.append("^XFFORMAT^FS")  # label home position
-        lines.append("^FN1^FD" + process_name + "^FS")  # this is readable
-        lines.append("^XZ")
-    return lines
+        format_lines.append("^FO20,30^AFN 78,39^FN1^FS")  # Larger font for short names
+    data_lines = [
+        f"^FN1^FD{process_name}^FS"  # Assign process_name to field 1 (human readable)
+    ]
+    return build_zpl_format(format_lines, data_lines, copies)
 
 
 @epp_decorator(script_path=__file__, timestamp=TIMESTAMP)
