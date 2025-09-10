499a7fb3b6157490f38d46589fa4aff7ea3356e9
FranBonath <franziska.bonath@scilifelab.se>
2025-09-10 14:12:54 +0200

    WIP upload manifest aviti epp

---
+++ scripts/upload_manifest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+
+
+if __name__ == "__main__":
+    # Parse args
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--pid",
+        required=True,
+        type=str,
+        help="Lims ID for current Process.",
+    )
+    parser.add_argument(
+        "--log",
+        required=True,
+        type=str,
+        help="Which file slot to use for the script log.",
+    )
+    parser.add_argument(
+        "--file",
+        required=True,
+        type=str,
+        help="Which file slot to take the run manifest from.",
+    )
+    args = parser.parse_args()
+
+    main(args)
