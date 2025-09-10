fa1dce2f39a0707be723915c477e6bd5bf9f1667
FranBonath <franziska.bonath@scilifelab.se>
2025-09-10 17:36:56 +0200

    WIP_making_zip_file

--- scripts/upload_manifest.py
+++ scripts/upload_manifest.py
@@ -17,8 +17,23 @@ def main(args: Namespace):
     lims = Lims(BASEURI, USERNAME, PASSWORD)
     process = Process(lims, id=args.pid)
 
-    message = f"I am perfct {process.type.name}"
-    print(message)
+    matching_file_slots = [
+        art
+        for art in process.all_outputs()
+        if art.name == args.file and art.type == "ResultFile"
+    ]
+
+    assert (
+        len(matching_file_slots) == 1
+    ), f"Could not find single file slot matching to '{args.file}'."
+    matching_file_slot = matching_file_slots[0]
+    assert matching_file_slot.files, f"'{matching_file_slot.name}' file slot is empty."
+    file_id = matching_file_slot.files[0].id
+    manifest_file_name = matching_file_slot.files[0].original_location
+    manifest_file_contents = lims.get_file_contents(id=file_id)
+
+    # Write and zip manifest(s)
+    zip_file = manifest_file_name.replace(".csv", ".zip")
 
 
 if __name__ == "__main__":
